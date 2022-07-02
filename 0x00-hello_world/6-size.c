05bbda15013c4170ba587b81a25886d45a16403e
Anietie22 <anietiecaleb@yahoo.com>
2022-07-02 15:13:26 -0700

    alx-low_level_programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * main - print the size of various types
+ * Description: prints the size of various types on the computer it is compiled and run on
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", (int) sizeof(char));
@@ -6,6 +11,5 @@ int main(void)
 	printf("Size of a long int: %d byte(s)\n", (int) sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n", (int) sizeof(long long int));
 	printf("Size of a float: %d byte(s)\n", (int) sizeof(float));
-
-		return (0);
+	return (0);
 }
